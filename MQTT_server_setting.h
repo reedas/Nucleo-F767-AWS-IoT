01b2314fda0ba5dcd3b6a481b242619e98435875
Andrew Reed <asr@btinternet.com>
2020-07-10 08:48:15 +0100

    Dummy Server Settings include file

---
+++ MQTT_server_setting.h
@@ -0,0 +1,64 @@
+#ifndef __MQTT_SERVER_SETTING_H__
+#define __MQTT_SERVER_SETTING_H__
+
+const char MQTT_SERVER_HOST_NAME[] = "a33c7qbzm0wymk-ats.iot.us-west-2.amazonaws.com";
+const char MQTT_CLIENT_ID[] = "AwsIoT_AR3";
+const char MQTT_USERNAME[] = {0};
+const char MQTT_PASSWORD[] = {0};
+const char MQTT_TOPIC_PUB[] = "$aws/things/City_AR_Thing3/shadow/update";
+const char MQTT_TOPIC_SUB[] = "$aws/things/City_AR_Thing3/shadow/update/accepted";
+
+
+const int MQTT_SERVER_PORT = 8883;
+
+/*
+ * Root CA certificate here in PEM format.
+ * "-----BEGIN CERTIFICATE-----\n"
+ * ...
+ * "-----END CERTIFICATE-----\n";
+ */
+ /* Amazon Root CA 1 */
+const char* SSL_CA_PEM ="-----BEGIN CERTIFICATE-----\n" \
+"MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF\n" \
+"ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6\n" \
+"b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL\n" \
+"MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv\n" \
+"b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj\n" \
+"ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM\n" \
+"9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw\n" \
+"IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6\n" \
+"VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L\n" \
+"93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm\n" \
+"jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC\n" \
+"AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA\n" \
+"A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI\n" \
+"U5PMCCjjmCXPI6T53iHTfIUJrU6adTrCC2qJeHZERxhlbI1Bjjt/msv0tadQ1wUs\n" \
+"N+gDS63pYaACbvXy8MWy7Vu33PqUXHeeE6V/Uq2V8viTO96LXFvKWlJbYK8U90vv\n" \
+"o/ufQJVtMVT8QtPHRh8jrdkPSHCa2XV4cdFyQzR1bldZwgJcJmApzyMZFo6IQ6XU\n" \
+"5MsI+yMRQ+hDKXJioaldXgjUkK642M4UwtBV8ob2xJNDd2ZhwLnoQdeXeGADbkpy\n" \
+"rqXRfboQnoZsG4q5WTP468SQvvG5\n" \
+"-----END CERTIFICATE-----\n";
+/* */
+/*
+ * (optional) Client certificate here in PEM format.
+ * Set NULL if you don't use.
+ * "-----BEGIN CERTIFICATE-----\n"
+ * ...
+ * "-----END CERTIFICATE-----\n";
+ */
+const char* SSL_CLIENT_CERT_PEM =  "-----BEGIN CERTIFICATE-----\n" \
+"-----END CERTIFICATE-----\n";
+
+
+/*
+ * (optional) Client private key here in PEM format.
+ * Set NULL if you don't use.
+ * "-----BEGIN RSA PRIVATE KEY-----\n"
+ * ...
+ * "-----END RSA PRIVATE KEY-----\n";
+ */
+const char* SSL_CLIENT_PRIVATE_KEY_PEM = "-----BEGIN RSA PRIVATE KEY-----\n" \
+"-----END RSA PRIVATE KEY-----\n";
+
+
+#endif /* __MQTT_SERVER_SETTING_H__ */
